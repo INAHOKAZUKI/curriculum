f0ccc5fc93e8d32033f112b83fbf5fafa51b182d
INAHOKAZUKI <calminaho@gmail.com>
2022-08-23 11:55:14 +0900

    第１章チェック課題
    Check課題の提出

---
+++ JavaStudy/Check1/Check.java
@@ -0,0 +1,28 @@
+/**
+ * バブルソート
+ * チェックテスト-Java1章
+ *
+ */
+ import java.util.Arrays;
+public class Check{
+    public static void main(String[] args){
+        /*
+        * 問1
+        * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
+        */
+        int data[] = {3, 1, 2, 7, 5};
+        /*
+        * 問2
+        * 以下のfor文を完成させなさい
+        */
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        Arrays.sort(data);
+        for(int j = 0; j < data.length;j++){
+            System.out.print(data[j] + " ");
+        }
+        
+    }
+}
